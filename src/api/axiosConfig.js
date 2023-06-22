807392abfbdbcbde9ca64b6db0a50adc7ec2bfec
Prashanth DP <1296prashanth@gmail.com>
2023-06-22 20:25:14 +0530

    Updating Backend API Baseurl-which is hosted on AWS

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://localhost:8080/',
+    // baseURL:'http://localhost:8080/',
+    baseURL: 'http://movieapi-application-env.eba-rfih6tad.eu-west-1.elasticbeanstalk.com/',
     headers: {"skip-browser-warning": "true"}
 });
